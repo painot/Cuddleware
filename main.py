84e9b65833f2a23aa8545b140fbc2546daa14daf
Paint <142173883+painot@users.noreply.github.com>
2025-04-01 23:16:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-# Cuddles
+# Painot, 30/03/2025, 20:00
+# Cuddleware - A module for cute mathematical cuddles, snuggles and tickles!
 
-print("Hello world!")
-print("Oopsie daisy!")
+import math
+import pyautogui
+import time
+import os
+import random
+
+# print("[Cuddleware] Started <3")
 
 Version = 1
+
+def lerp(a, b, t):
+    return a + (b - a) * t
+
+def clamp(min_value, max_value, value):
+    return max(min(value, max_value), min_value)
+
+def OneNotePages(list):
+    for entry in list:
+        pyautogui.hotkey('ctrl', 'n')
+        pyautogui.write(entry)
+        time.sleep(0.1)
+        pyautogui.press('enter')
+        time.sleep(0.1)
+
+if __name__ == "__main__":
+    print("You are not supposed to run Cuddleware directly!")
