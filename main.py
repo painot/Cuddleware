30fdbedfa9479d6beaa264abe6697c70a371c966
Paint <142173883+painot@users.noreply.github.com>
2025-04-01 23:25:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,33 @@ import pyautogui
 import time
 import os
 import random
+import numpy
+import scipy
 
 # print("[Cuddleware] Started <3")
 
 Version = 1
+
+# MATH
 
 def lerp(a, b, t):
     return a + (b - a) * t
 
 def clamp(min_value, max_value, value):
     return max(min(value, max_value), min_value)
+
+def lambertW(x):
+    return scipy.special.lambertw(x).real
+def gamma(x):
+    return scipy.special.gamma(x)
+
+def code(values, code):
+    return [value * code for value in values]
+
+def decode(values, code):
+    return [value / code for value in values]
+
+# MISC
 
 def OneNotePages(list):
     for entry in list:
@@ -26,4 +43,5 @@ def OneNotePages(list):
         time.sleep(0.1)
 
 if __name__ == "__main__":
-    print("You are not supposed to run Cuddleware directly!")
+    # print("You are not supposed to run Cuddleware directly!")
+    pass
